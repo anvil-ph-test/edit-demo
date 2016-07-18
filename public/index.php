f284246e2427d726cd77c1ec029d42d2131e6d0e
Meredydd Luff <meredydd@senatehouse.org>
2016-07-19 00:30:06 +0100

    Prototype app

--- public/index.php
+++ public/index.php
@@ -1,3 +1,48 @@
 <?php
-echo 'Hello from the public directory';
+$cfg_str = file_get_contents("../config.json");
+$cfg = json_decode($cfg_str, true);
+
+# The Internet gets to set these values. Be conservative.
+function s($str) { return preg_replace("([^A-Za-z0-9 \#]+)", "", $str); }
+function scfg($name) { global cfg; return s($cfg[$name]); }
+
+$s_user_name = htmlspecialchars($_POST['name']);
 ?>
+<html>
+<head><title>Live configuration demo</title></head>
+
+<style>
+	body {
+		font-size: 14px;
+		font-family: <?=scfg('font')?>;
+		background-color: <?=scfg('background')?>;
+	}
+
+	.name {
+		font-size: 18px;
+	}
+
+	.refresh-link a {
+		font-size: 12px;
+		color: #000;
+		padding-bottom: 14px;
+	}
+</style>
+<?php
+?>
+
+<body>
+
+<center class="refresh-link"><a href="?x=<?=rand()?>">Refresh</a></center>
+
+<b>Please enter your name:</b><br>
+<form action="index.php" method="POST"><input type="text" name="name" value="<?=$s_user_name?>"><input type="submit" value="Say Hello"></form>
+
+<div class="name">
+<? if ($user_name) { ?>
+Hello, <?= ($cfg['uppercase']?strtoupper($s_user_name):$_user_name) ?>!
+<? } ?>
+
+</body>
+</html>
+
